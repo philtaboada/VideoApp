aa6ad6b259d1a9cf96d57c325f2addbaf35c1480
Philtaboada <philtaboada1997@gmail.com>
2023-01-13 01:33:50 -0500

    videoApp

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import { Stack } from "@mui/material"
+import datos from '../utils/data.json'
+
+const Sidebar = ({ setSelectedCategory, selectedCategory }) => {
+    return (
+        <Stack
+            direction='row'
+            sx={{
+                overflow: 'auto',
+                height: { sx: 'auto', md: '95%' },
+                flexDirection: { md: "column" },
+            }}
+        >
+            {datos.map((category) =>
+                <button
+                    className="category-btn"
+                    onClick={() => setSelectedCategory(category.name)}
+                    style={{
+                        background: category.name === selectedCategory && '#FC1503', color: 'white'
+                    }}
+
+                    key={category.name}
+                >
+                    <i style={{
+                        color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'
+                    }} className={category.icon}></i>
+                    <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+                </button>
+            )
+            }
+        </Stack>
+    )
+}
+
+export default Sidebar
---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,20 @@
+
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+        maxResults: 50,
+    },
+    headers: {
+        'X-RapidAPI-Key': '33787b9f8emsh8158008a0f4169ap16190bjsna00fc2a2d4e2',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
+};
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+};
